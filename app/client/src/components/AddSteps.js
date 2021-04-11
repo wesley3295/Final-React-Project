62f89c4cd6136132fb2dfcc83ab36de961057e7e
Wesley <wspencer3295@yahoo.com>
2021-04-11 13:12:30 -0700

    refactor

---
+++ app/client/src/components/AddSteps.js
@@ -0,0 +1,20 @@
+// import TextField from "@material-ui/core/TextField";
+
+// const AddSteps = function(props){
+//     const { } = props;
+//     return (
+//       <div>
+//           <TextField
+//             id={`step-${idx}`}
+//             label={`Step ${idx}`}
+//             name="instructions"
+//             multiline
+//             rows={10}
+//             defaultValue={diy.instructions}
+//             onChange={handleChange}
+//             variant="outlined"
+//             />
+//       </div>
+//     );
+//   }
+//   export default AddSteps
