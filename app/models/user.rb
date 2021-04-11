71da62d310c866f57e897d11ea1c615080e89c09
Wesley <wspencer3295@yahoo.com>
2021-04-11 13:18:49 -0700

    fixed bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
     validates :username, presence: true
-validates :username, uniqueness: true
-validates :username, length: { minimum: 4 }
-validates :email, presence: true
-validates :email, uniqueness: true
+# validates :username, uniqueness: true
+# validates :username, length: { minimum: 4 }
+# validates :email, presence: true
+# validates :email, uniqueness: true
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
     has_secure_password
     has_many :diys
--- db/migrate/20210407213521_add_instructions_to_diy.rb
+++ db/migrate/20210407213521_add_instructions_to_diy.rb
@@ -1,6 +1,5 @@
 class AddInstructionsToDiy < ActiveRecord::Migration[6.1]
   def change
     add_column :diys, :instructions, :text, array:true, default: []
-    add_column :diys, :instructions, :text, array:true, default: []
   end
 end
